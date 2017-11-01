d04c615f676773d9122947b5c524d2dc7a822957
xenotime-india <sandeepkhoj@gmail.com>
2017-11-01 16:05:43 +0530

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/CV/app-8f3eb459691719e026e6.js","f05c8f0918bfe345207de5ebe9d22e67"],["/CV/app-cb1c77bb3aa48cc8541f.js","a33cf0932f267e30deb1c4b731223ded"],["/CV/commons-3a4603e18850c0772a72.js","07a41891b9dddfb07ab2b4057d42d57a"],["/CV/index.html","a58925758633cecec4573c9d45021634"],["/CV/offline-plugin-app-shell-fallback/index.html","30f4068c67c65f5accead1ae28069bde"]];
+var precacheConfig = [["/CV/app-8f3eb459691719e026e6.js","f05c8f0918bfe345207de5ebe9d22e67"],["/CV/app-cb1c77bb3aa48cc8541f.js","a33cf0932f267e30deb1c4b731223ded"],["/CV/commons-3a4603e18850c0772a72.js","07a41891b9dddfb07ab2b4057d42d57a"],["/CV/index.html","6c0411430846bb4c44d44812fa94dce9"],["/CV/offline-plugin-app-shell-fallback/index.html","becfc624d358770d22d76560f4446ff2"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
